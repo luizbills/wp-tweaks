624b183fec2ee8b75b22b922dd8c6950d7299195
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-08-04 14:46:46 -0300

    typo

--- inc/tweaks/disable-public-rest-api.php
+++ inc/tweaks/disable-public-rest-api.php
@@ -8,7 +8,7 @@
 add_filter( 'rest_authentication_errors', 'wp_tweaks_disable_rest_api' );
 function wp_tweaks_disable_rest_api ( $flag ) {
 	if ( ! is_user_logged_in() ) {
-		$flag = new WP_Error( 'rest-api-disabled', 'WP Rest API Disabled for public access' );
+		$flag = new WP_Error( 'rest-api-disabled', 'WP REST API Disabled for public access' );
 	}
 	return $flag;
 }
