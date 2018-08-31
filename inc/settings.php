50328ad467fda57b2706f146d5a939069ecc3bc7
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-08-31 15:00:20 -0300

    change jquery default version to 2.2.4

--- inc/settings.php
+++ inc/settings.php
@@ -114,10 +114,11 @@ class WP_Tweaks_Settings {
 			'_jquery-version' => [
 				'label' => esc_html__( 'jQuery version', 'wp-tweaks' ),
 				'type' => 'text',
-				'default' => '3.3.1',
+				'default' => '2.2.4',
 				'props' => [
 					'class' => 'small-text'
-				]
+				],
+				'desc' => esc_html__( 'current stable versions: ', 'wp-tweaks' ) . '`1.12.4`, `2.2.4` or `3.3.1`.',
 			],
 			'remove-admin-bar-logo' => [
 				'label' => esc_html__( 'Remove WordPress logo from admin bar', 'wp-tweaks' ),
