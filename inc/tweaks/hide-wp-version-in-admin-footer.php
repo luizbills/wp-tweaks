ab7141646851b77ab19d8ba4eb9ffda37da92148
Luiz Paulo "Bills <1798830+luizbills@users.noreply.github.com>
2018-10-16 14:48:02 -0300

    Create hide-wp-version-in-admin-footer.php

---
+++ inc/tweaks/hide-wp-version-in-admin-footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Hide WordPress version in admin footer
+ *
+ * @package wp-tweaks
+ */
+
+add_action( 'admin_menu', 'wp_tweaks_hide_version_in_admin_footer' );
+function wp_tweaks_hide_version_in_admin_footer () {
+    if ( ! current_user_can( 'install_plugins' ) ) {
+        remove_filter( 'update_footer', 'core_update_footer' ); 
+    }
+}
