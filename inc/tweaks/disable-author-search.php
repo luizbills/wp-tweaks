2a115fbe4bbfeea797117e0aba5bc8dd0dab8ad1
Luiz "Bills <luizbills@pm.me>
2021-03-26 09:15:22 -0300

    Update disable-author-search.php

--- inc/tweaks/disable-author-search.php
+++ inc/tweaks/disable-author-search.php
@@ -7,15 +7,10 @@
 
 add_action( 'wp', 'wp_tweaks_disable_author_search' );
 function wp_tweaks_disable_author_search () {
-	$disable_author_page = apply_filters( 'wp_tweaks_disable_author_page', true );
+	global $wp_query;
 	$disable_author_query = apply_filters( 'wp_tweaks_disable_author_query', true );
 
-	global $wp_query;
-
 	if ( $disable_author_query && isset( $_GET['author'] ) ) {
-		$wp_query->set_404();
-		status_header( 404 );
-	} else if ( $disable_author_page && is_author() ) {
 		$wp_query->set_404();
 		status_header( 404 );
 	}
