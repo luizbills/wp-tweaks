86d87dce6e3f2b7c43e4907bfc70c24720d64038
Luiz "Bills <luizbills@pm.me>
2021-03-26 12:25:26 +0000

    fix disallow file edit setting

--- inc/tweaks/disallow-file-edit.php
+++ inc/tweaks/disallow-file-edit.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'init','wp_tweaks_disable_file_edit' );
+add_action( 'init', 'wp_tweaks_disallow_file_edit' );
 function wp_tweaks_disallow_file_edit () {
 	if ( ! defined( 'DISALLOW_FILE_EDIT' ) ) define( 'DISALLOW_FILE_EDIT', true );
 }
