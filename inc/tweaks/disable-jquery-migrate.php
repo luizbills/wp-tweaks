2acd5704f898c5bd37e65139dbd2b302cb77960b
Luiz Bills <luizbills@pm.me>
2023-01-10 12:47:25 -0300

    disable jquery migrate

---
+++ inc/tweaks/disable-jquery-migrate.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Disable the jquery-migrade JavaScript.
+ *
+ * @package wp-tweaks
+ */
+
+if ( is_admin() ) return;
+
+add_action( 'wp_print_scripts', 'wp_tweaks_dequeue_jquery_migrate' );
+function wp_tweaks_dequeue_jquery_migrate () {
+    global $wp_scripts;
+    if ( isset( $wp_scripts->registered['jquery'] ) ) {
+        $jquery = $wp_scripts->registered['jquery'];
+        if ( ! empty( $jquery->deps ) ) {
+            $jquery->deps = array_diff(
+                $jquery->deps,
+                [ 'jquery-migrate' ]
+            );
+        }
+    }
+}
