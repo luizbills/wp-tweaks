6a3d4491acb7f6dffaa69e40eea6a12535501cc4
Luiz Bills <luizbills@pm.me>
2023-01-10 12:46:47 -0300

    trim whitespaces

--- inc/tweaks/hide-wp-version-in-admin-footer.php
+++ inc/tweaks/hide-wp-version-in-admin-footer.php
@@ -8,6 +8,6 @@
 add_action( 'admin_menu', 'wp_tweaks_hide_version_in_admin_footer' );
 function wp_tweaks_hide_version_in_admin_footer () {
     if ( ! current_user_can( 'install_plugins' ) ) {
-        remove_filter( 'update_footer', 'core_update_footer' ); 
+        remove_filter( 'update_footer', 'core_update_footer' );
     }
 }
