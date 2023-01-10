9cce259dec067693c6108bcec05becff262a4b60
Luiz Bills <luizbills@pm.me>
2023-01-10 12:47:36 -0300

    add {copyright} placeholder

--- inc/tweaks/custom-admin-footer-text.php
+++ inc/tweaks/custom-admin-footer-text.php
@@ -6,17 +6,18 @@
  */
 
 add_filter( 'admin_footer_text', 'wp_tweaks_custom_admin_footer_text' );
-function wp_tweaks_custom_admin_footer_text ( $content ) {
-	$text = WP_Tweaks_Settings::get_option( 'custom-admin-footer-text' );
-	if ( $text ) {
-		$placeholders = apply_filters( 'wp_tweaks_admin_footer_placeholders', [
-			'{current_year}' => date('Y'),
-			'{site_name}' => get_bloginfo('name')
-		] );
-		foreach ( $placeholders as $placeholder => $value ) {
-			$text = str_replace( $placeholder, $value, $text );
-		}
-		$content = apply_filters( 'wp_tweaks_admin_footer_text', $text );
+function wp_tweaks_custom_admin_footer_text () {
+	$text_template = WP_Tweaks_Settings::get_option( 'custom-admin-footer-text' );
+	$text = $text_template;
+	$placeholders = apply_filters( 'wp_tweaks_admin_footer_placeholders', [
+		'{copyright}' => '&copy;',
+		'{current_year}' => date('Y'),
+		'{site_name}' => get_bloginfo('name')
+	] );
+
+	foreach ( $placeholders as $placeholder => $value ) {
+		$text = str_replace( $placeholder, $value, $text );
 	}
-	return $content;
+
+	echo apply_filters( 'wp_tweaks_admin_footer_text', $text );
 }
