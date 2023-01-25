def3ca5b75c69d6a958e4df12c8fd08417ce2630
Luiz Bills <luizbills@pm.me>
2023-01-25 11:38:48 -0300

    fix some tweaks

--- inc/tweaks/custom-admin-footer-text.php
+++ inc/tweaks/custom-admin-footer-text.php
@@ -8,7 +8,7 @@ if ( ! defined( 'WPINC' ) ) die();
 
 add_filter( 'admin_footer_text', 'wp_tweaks_custom_admin_footer_text' );
 function wp_tweaks_custom_admin_footer_text () {
-	$text_template = WP_Tweaks_Settings::get_option( 'custom-admin-footer-text' );
+	$text_template = WP_Tweaks::get_option( 'custom-admin-footer-text' );
 	$text = $text_template;
 	$placeholders = apply_filters( 'wp_tweaks_admin_footer_placeholders', [
 		'{copyright}' => '&copy;',
--- inc/tweaks/security-headers.php
+++ inc/tweaks/security-headers.php
@@ -8,7 +8,7 @@ if ( ! defined( 'WPINC' ) ) die();
 
 add_action( 'wp_headers', 'wp_tweaks_security_headers' );
 function wp_tweaks_security_headers ( $headers ) {
-	$options = WP_Tweaks_Settings::get_option( 'security-headers' );
+	$options = WP_Tweaks::get_option( 'security-headers' );
 	$values = [
 		'strict-transport-security' => 'max-age=31536000; includeSubDomains; preload',
 		'x-content-type-options' => 'nosniff',
