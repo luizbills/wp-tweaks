c9a6868417fbaddea4d86279f9eb90ae2c276f48
Luiz Bills <luizbills@pm.me>
2023-03-14 12:10:01 -0300

    add \esc_unsafe_html()

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,9 +1,24 @@
 <?php
 
 if ( ! function_exists( 'is_login_page' ) ) :
-
+/**
+ * @return bool
+ */
 function is_login_page () {
 	return in_array( $GLOBALS['pagenow'], [ 'wp-login.php', 'wp-register.php' ] );
 }
+endif;
 
+if ( ! function_exists( 'esc_unsafe_html' ) ) :
+/**
+ * @param string $html
+ * @return string
+ */
+function esc_unsafe_html ( $html ) {
+	// remove all script and style tags with code
+	$html = \preg_replace( '/<(script|style)[^>]*?>.*?<\/\\1>/si', '', $html );
+	// remove any script, style, link and iframe tags
+	$html = \preg_replace( '/<(script|style|iframe|link)[^>]*?>/si', '', $html );
+	return $html;
+}
 endif;
