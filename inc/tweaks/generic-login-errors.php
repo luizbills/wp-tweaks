212dd65ab78652e5f957b6177ea949f786d8e80a
Luiz Bills <luizbills@pm.me>
2023-03-21 18:44:49 -0300

    improve generic login errors

--- inc/tweaks/generic-login-errors.php
+++ inc/tweaks/generic-login-errors.php
@@ -6,7 +6,20 @@
  */
 if ( ! defined( 'WPINC' ) ) die();
 
-add_filter( 'login_errors', 'wp_tweaks_login_errors', 1 );
+add_filter( 'wp_login_errors', 'wp_tweaks_login_errors', 99 );
 function wp_tweaks_login_errors ( $errors ) {
-	return '<strong>' . __( 'ERROR', 'wp-tweaks' ) . '</strong>: ' . esc_html__( 'Incorrect email address or password.', 'wp-tweaks' );
+	if ( ! is_wp_error( $errors ) ) return new WP_Error();
+
+	$errors->remove( 'empty_username' );
+	$errors->remove( 'invalid_username' );
+	$errors->remove( 'empty_password' );
+	$errors->remove( 'incorrect_password' );
+	$errors->remove( 'invalid_email' );
+
+	$errors->add(
+		'invalid_login',
+		esc_html__( 'Incorrect email address or password.', 'wp-tweaks' )
+	);
+
+	return $errors;
 }
