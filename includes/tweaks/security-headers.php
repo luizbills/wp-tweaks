f14920414d0738dc898dc9ee00e6f8da5a8ff807
Luiz Bills <luizbills@pm.me>
2023-09-20 12:18:22 -0300

    add referrer-policy header
    - default: strict-origin-when-cross-origin

--- includes/tweaks/security-headers.php
+++ includes/tweaks/security-headers.php
@@ -13,10 +13,11 @@ function wp_tweaks_security_headers ( $headers ) {
 		'wp_tweaks_security_headers_values',
 		[
 			'strict-transport-security' => 'max-age=31536000; includeSubDomains; preload',
-			'x-content-type-options' => 'nosniff',
-			'x-xss-protection' => '1; mode=block',
-			'content-security-policy' => "default-src https:; font-src https: data:; img-src https: data:; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'",
-			'x-frame-options' => 'sameorigin'
+			'x-content-type-options'    => 'nosniff',
+			'x-xss-protection'          => '1; mode=block',
+			'content-security-policy'   => "default-src https:; font-src https: data:; img-src https: data:; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'",
+			'x-frame-options'           => 'sameorigin',
+			'referrer-policy'           => 'strict-origin-when-cross-origin',
 		]
 	);
 
