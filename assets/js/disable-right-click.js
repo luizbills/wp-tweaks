7c55e2e1908f3f1e085bbefd5f9a651f45326547
Luiz Bills <luizbills@pm.me>
2023-10-01 14:13:13 -0300

    minify js

--- assets/js/disable-right-click.js
+++ assets/js/disable-right-click.js
@@ -1,7 +1,1 @@
-{
-  const e = document.body;
-  e.addEventListener("contextmenu", (e) => e.preventDefault()),
-    e.addEventListener("copy", (e) => e.preventDefault()),
-    e.addEventListener("cut", (e) => e.preventDefault()),
-    e.addEventListener("paste", (e) => e.preventDefault());
-}
+{const e=document.body;e.addEventListener("contextmenu",(e=>e.preventDefault())),e.addEventListener("copy",(e=>e.preventDefault())),e.addEventListener("cut",(e=>e.preventDefault())),e.addEventListener("paste",(e=>e.preventDefault()))}
